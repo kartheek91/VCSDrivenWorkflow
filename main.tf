a9c6a4cdfb458fe66ee1736695ec246f0efd63f8
G S S KARTHEEK <kartheek@sails.solutions>
2024-10-24 16:22:29 +0530

    Module Changes

--- main.tf
+++ main.tf
@@ -9,13 +9,13 @@ resource "azurerm_resource_group" "rg" {
 }
 
 # Working with Private Modules
-# module "storageaccount" {
-#   source  = "app.terraform.io/kartheek91/storageaccount/azure"
-#   version = "0.0.2"
-#   # insert required variables here
-#   location = var.location
-#   resource_group_name = azurerm_resource_group.rg.name
-#   storage_account_name = var.storage_account_name
-#   project = var.project
-#   owner = var.owner
-# }
+module "storageaccount" {
+  source  = "app.terraform.io/kartheek91/storageaccount/azure"
+  version = "0.0.2"
+  # insert required variables here
+  location = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  storage_account_name = var.storage_account_name
+  project = var.project
+  owner = var.owner
+}
