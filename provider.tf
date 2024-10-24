0cdd5bc08bf84d0222edcce49ebb7ccb2333ea4b
G S S KARTHEEK <kartheek@sails.solutions>
2024-10-24 10:52:37 +0530

    Changes

---
+++ provider.tf
@@ -0,0 +1,22 @@
+terraform {
+    cloud {
+      organization = "kartheek91"
+      workspaces {
+        name = "VCSDrivenWorkflow"
+      }
+    }
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.6.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  client_id = var.client_id
+  client_secret = var.client_secret
+  subscription_id = var.subscription_id
+  tenant_id = var.tenant_id
+}
---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "resoure_group_name" {
+  type = string
+}
+variable "location" {
+  type = string
+}
+variable "client_id" {
+  type = string
+}
+variable "tenant_id" {
+  type = string
+}
+variable "client_secret" {
+  type = string
+}
+variable "subscription_id" {
+  type = string
+}
+variable "owner" {
+  type = string
+}
+# variable "project" {
+#   type = string
+# }
