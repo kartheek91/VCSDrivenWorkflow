30509f6ce4dd334428b0f10de12fbdd9674c9b4b
G S S KARTHEEK <kartheek@sails.solutions>
2024-10-24 11:32:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,3 +7,12 @@ resource "azurerm_resource_group" "rg" {
     owner = var.owner
   }
 }
+
+module "storageaccount" {
+  source  = "app.terraform.io/kartheek91/storageaccount/azure"
+  version = "0.0.1"
+  # insert required variables here
+  location = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  storage_account_name = var.storage_account_name
+}
