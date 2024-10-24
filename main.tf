6333f538d02c5cee2bd4677fb4d946083d001890
G S S KARTHEEK <kartheek@sails.solutions>
2024-10-24 15:17:38 +0530

    c

--- main.tf
+++ main.tf
@@ -2,10 +2,10 @@ resource "azurerm_resource_group" "rg" {
   name     = var.resoure_group_name
   location = var.location
 
-  tags = {
-    project = var.project
-    owner = var.owner
-  }
+  # tags = {
+  #   project = var.project
+  #   owner = var.owner
+  # }
 }
 
 # Working with Private Modules
