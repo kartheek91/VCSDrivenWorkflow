f14a1e558a95eb84e7899127011fa70f732710b6
G S S KARTHEEK <kartheek@sails.solutions>
2024-10-24 11:44:16 +0530

    sa

--- main.tf
+++ main.tf
@@ -8,11 +8,12 @@ resource "azurerm_resource_group" "rg" {
   }
 }
 
-module "storageaccount" {
-  source  = "app.terraform.io/kartheek91/storageaccount/azure"
-  version = "0.0.1"
-  # insert required variables here
-  location = var.location
-  resource_group_name = azurerm_resource_group.rg.name
-  storage_account_name = var.storage_account_name
-}
+# Working with Private Modules
+# module "storageaccount" {
+#   source  = "app.terraform.io/kartheek91/storageaccount/azure"
+#   version = "0.0.1"
+#   # insert required variables here
+#   location = var.location
+#   resource_group_name = azurerm_resource_group.rg.name
+#   storage_account_name = var.storage_account_name
+# }
